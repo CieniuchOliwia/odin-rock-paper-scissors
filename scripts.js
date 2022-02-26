07e1835a6925e6dc4242bff321fd949d12387808
Oliwia <cieniuch.oliwia@gmail.com>
2022-02-26 15:35:54 +0100

    add buttons

--- scripts.js
+++ scripts.js
@@ -6,19 +6,23 @@ function computerChoice() {
 
 }
 
-function playerChoice(msg='Choose Paper, Rock or Scissors') {
-    let myChoice = prompt(msg).toLowerCase();
-    if (options.includes(myChoice)) {
-        return myChoice;
-    } else {
-        return playerChoice('Invalid input.Choose again!');
-    }
 
-}
-function compare() {
-    let player=playerChoice();
-    let computer=computerChoice();
-    console.log(computer);
+//function playerChoice(msg='Choose Paper, Rock or Scissors') {
+    //let myChoice = prompt(msg).toLowerCase();
+    //if (options.includes(myChoice)) {
+        //return myChoice;
+    //} else {
+        //return playerChoice('Invalid input.Choose again!');
+    //}
+
+//}
+
+
+function compare(player,computer) {
+    //let player=playerChoice();
+    //let player=playerChoice;
+    //let computer=computerChoice();
+    //console.log(computer);
 
     if (player == computer) {
         return "draw";
@@ -41,8 +45,48 @@ function compare() {
         return 'You win.';
     }
 
-
-
 }
 
-    
+
+const buttonRock=document.createElement('button');
+buttonRock.classList.add('buttons');
+buttonRock.textContent="ROCK";
+document.body.appendChild(buttonRock);
+buttonRock.addEventListener('click', function(){
+    let choice=computerChoice();
+    const result=document.createElement('div');
+    result.textContent='Computer choice: '+choice+'. '+compare('rock',choice);
+   document.body.appendChild(result);
+}
+);
+ 
+
+
+
+const buttonPaper=document.createElement('button');
+buttonPaper.classList.add('buttons');
+buttonPaper.textContent="PAPER";
+document.body.appendChild(buttonPaper);  
+buttonPaper.addEventListener('click', function(){
+    let choice=computerChoice();
+    const result=document.createElement('div');
+    result.textContent='Computer choice: '+choice+'. '+compare('paper',choice);
+   document.body.appendChild(result);
+}
+);
+
+
+const buttonScissors =document.createElement('button');
+buttonScissors.classList.add('buttons');
+buttonScissors.textContent="SCISSORS";
+document.body.appendChild(buttonScissors);  
+buttonScissors.addEventListener('click', function(){
+    let choice=computerChoice();
+    const result=document.createElement('div');
+    result.textContent='Computer choice: '+choice+'. '+compare('scissors',choice);
+   document.body.appendChild(result);
+}
+);
+
+
+
